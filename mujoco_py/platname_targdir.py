a1791d1b1737efbf2510e788ec827a088c6e45c2
bstee615 <benjaminjsteenhoek@gmail.com>
2021-02-27 02:23:35 -0600

    Apply fixes from https://stackoverflow.com/a/38839741

--- mujoco_py/platname_targdir.py
+++ mujoco_py/platname_targdir.py
@@ -3,7 +3,7 @@ if sys.platform.startswith("darwin"):
     platname = "osx"
 elif sys.platform.startswith("linux"):
     platname = "linux"
-elif sys.platform.startswith("windows"):
+elif sys.platform.startswith("windows") or sys.platform.startswith("win32"):
     platname = "win"
 targdir = "mujoco_%s"%platname
 
